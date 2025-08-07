536954a6c44c3e29a35e5b3d96ccc146ea57eb0b
Gerbrand Bosch <gerbrandb@gmail.com>
2025-08-07 10:34:41 +0200

    Configurable environment and moved mtls setting to .env

--- public/index.php
+++ public/index.php
@@ -5,5 +5,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader);
 
+$env = getenv('SERVICES_ENVIRONMENT') ?: null;
 $services = json_decode(file_get_contents(__DIR__ . '/../services.json'), true);
+
+if($env){
+    if(!array_key_exists($env, $services)){
+        http_response_code(500);
+        echo 'Environment not found';
+        exit;
+    }
+    $services = [$env => $services[$env]];
+}
+
 echo $twig->render('index.twig', ['services' => $services]);
