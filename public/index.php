4422cd5ff8eb7f1541be22a6d2fa231d0b213ef8
Tom Keizer <tom@bustingbytes.nl>
2026-01-16 11:11:26 +0100

    feat: add mgo services support

--- public/index.php
+++ public/index.php
@@ -1,15 +1,39 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+declare(strict_types=1);
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
-$twig = new \Twig\Environment($loader);
+use Dotenv\Dotenv;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/util.php';
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+$twig = new Environment($loader);
 
 $env_path = getenv('ENV_PATH') ?: __DIR__ . '/..';
-$dotenv = Dotenv\Dotenv::createImmutable($env_path);
+$dotenv = Dotenv::createImmutable($env_path);
 $dotenv->load();
 
-$services = json_decode(file_get_contents(__DIR__ . '/../services.json'), true);
-$env = $_ENV['SERVICES_ENVIRONMENT']  ?: null;
+$services = getConfiguredServices();
+$configuredEnvironment = getEnvironmentFromConfig();
 
-echo $twig->render('index.twig', ['env' => $env, 'services' => $services]);
+$envs = [];
+if ($configuredEnvironment !== null) {
+    $envs = [$configuredEnvironment];
+} else {
+    $envSet = [];
+    foreach ($services as $service) {
+        if (!isset($service['environments']) || !is_array($service['environments'])) {
+            continue;
+        }
+        foreach (array_keys($service['environments']) as $envName) {
+            $envSet[$envName] = true;
+        }
+    }
+    $envs = array_keys($envSet);
+    sort($envs);
+}
+
+echo $twig->render('index.twig', ['envs' => $envs, 'services' => $services, 'app_name' => getAppName()]);
